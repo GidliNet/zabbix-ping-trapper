bbe4828cf37585ebab74e2ec763f5142369b1065
Ejayz <32452006+Ejayz@users.noreply.github.com>
2026-01-04 19:20:04 +0800

    Trapper Function update
    Added 2nd Host for dual host ping, also added 2nd Trapper for dual item collection

--- Dockerfile
+++ Dockerfile
@@ -21,14 +21,14 @@ RUN npm ci
 COPY . .
 
 # Set default environment variables (can be overridden)
-ENV IP=1.1.1.1
+ENV IP="1.1.1.1,8.8.8.8"
 ENV CRON="*/1 * * * * *"
 ENV PACKETLOSS_COUNT=60
 
 
 
 # Zabbix trapper as JSON string
-ENV ZABBIX_TRAPPER='[  {    "server": "172.16.4.150",    "host": "172.16.4.139",    "key": "Trapper.Ping"  },  {    "server": "172.16.4.150",    "host": "172.16.4.139",    "key": "Trapper.PacketLoss"  }]'
+ENV ZABBIX_TRAPPER='[{   "server": "172.16.4.150",    "host": "172.16.4.139",    "key": "Trapper.Ping"  },{"server": "172.16.4.150","host": "172.16.4.139",    "key": "Trapper.PacketLoss"  },{   "server": "172.16.4.150",    "host": "172.16.4.139",    "key": "Trapper.Ping"  },  {    "server": "172.16.4.150",    "host": "172.16.4.139",    "key": "Trapper.PacketLoss"  }]'
 
 EXPOSE 10051 
 EXPOSE 162
--- index.js
+++ index.js
@@ -4,39 +4,36 @@ const zabbix = require("zabbix-promise");
 const dotenv = require("dotenv");
 dotenv.config();
 
-
-
-const IP = process.env.IP || "1.1.1.1";
+const IP = process.env.IP.split(",") || "1.1.1.1";
 const CRON = process.env.CRON || "*/1 * * * * *";
 const PACKETLOSS_COUNT = Number(process.env.PACKETLOSS_COUNT) || 60;
 const ZABBIX_TRAPPER = JSON.parse(process.env.ZABBIX_TRAPPER) || [
   {
     server: "172.16.4.150",
     host: "172.16.4.139",
-    key: "Trapper.Ping",
+    key: "Trapper.Ping1",
   },
   {
     server: "172.16.4.150",
     host: "172.16.4.139",
-    key: "Trapper.PacketLoss",
+    key: "Trapper.PacketLoss1",
+  },
+  {
+    server: "172.16.4.150",
+    host: "172.16.4.139",
+    key: "Trapper.Ping2",
+  },
+  {
+    server: "172.16.4.150",
+    host: "172.16.4.139",
+    key: "Trapper.PacketLoss2",
   },
 ];
 
-console.log("Check env.")
-console.log("IP",IP)
-console.log("CRON",CRON)
-console.log("PACKETLOSS_COUNT:",PACKETLOSS_COUNT)
-console.log("Ping  Trapper:",{
-      server: ZABBIX_TRAPPER[0].server,
-      host: ZABBIX_TRAPPER[0].host,
-      key: ZABBIX_TRAPPER[0].key,
-    })
-    console.log("Packetloss  Trapper:",{
-      server: ZABBIX_TRAPPER[1].server,
-      host: ZABBIX_TRAPPER[1].host,
-      key: ZABBIX_TRAPPER[1].key,
-    })
-
+if (IP.length !== 2 || ZABBIX_TRAPPER.length !== 4) {
+  console.log("Exitting, Thus not met the required number of paramter for ZABBIX TRAPPER and IP");
+  return 0;
+}
 
 const session = ping.createSession({
   networkProtocol: ping.NetworkProtocol.IPv4,
@@ -46,14 +43,21 @@ const session = ping.createSession({
   timeout: 2000,
   ttl: 128,
 });
+
+
 let pingCount = 0;
 let pingFailed = 0;
 let totalTime = 0;
+
+
+let pingCount1 = 0;
+let pingFailed1 = 0;
+let totalTime1 = 0;
 cron.CronJob.from({
   cronTime: CRON,
   onTick: function () {
-    session.pingHost(IP, function (error, target, sent, rcvd) {
-        console.log(error, target, sent, rcvd)
+    session.pingHost(IP[0], function (error, target, sent, rcvd) {
+      console.log(error, target, sent, rcvd);
       Ping(rcvd - sent);
       if (error) {
         pingFailed++;
@@ -68,6 +72,33 @@ cron.CronJob.from({
         pingCount = 0;
         pingFailed = 0;
         totalTime = 0;
+      }
+    });
+  },
+  start: true,
+});
+
+
+
+cron.CronJob.from({
+  cronTime: CRON,
+  onTick: function () {
+    session.pingHost(IP[1], function (error, target, sent, rcvd) {
+      console.log(error, target, sent, rcvd);
+      SecondPing(rcvd - sent);
+      if (error) {
+        pingFailed1++;
+      } else {
+        pingCount1++;
+      }
+      totalTime1++;
+
+      if (totalTime1 == PACKETLOSS_COUNT) {
+        const percentage = (pingFailed1 / totalTime1) * 100;
+        SecondPacketLoss(`${percentage}`);
+        pingCount1 = 0;
+        pingFailed1 = 0;
+        totalTime1 = 0;
       }
     });
   },
@@ -93,6 +124,32 @@ const PacketLoss = async (data) => {
       server: ZABBIX_TRAPPER[1].server,
       host: ZABBIX_TRAPPER[1].host,
       key: ZABBIX_TRAPPER[1].key,
+      value: data,
+    });
+    console.log("Packetloss Trapper was sent");
+  } catch (error) {
+    console.log("Packetloss encountered an error:" + error);
+  }
+};
+const SecondPing = async (data) => {
+  try {
+    const result = await zabbix.sender({
+      server: ZABBIX_TRAPPER[2].server,
+      host: ZABBIX_TRAPPER[2].host,
+      key: ZABBIX_TRAPPER[2].key,
+      value: data,
+    });
+    console.log("Ping Trapper was sent");
+  } catch (error) {
+    console.log("Ping Trapper encountered an error:" + error);
+  }
+};
+const SecondPacketLoss = async (data) => {
+  try {
+    const result = await zabbix.sender({
+      server: ZABBIX_TRAPPER[3].server,
+      host: ZABBIX_TRAPPER[3].host,
+      key: ZABBIX_TRAPPER[3].key,
       value: data,
     });
     console.log("Packetloss Trapper was sent");
