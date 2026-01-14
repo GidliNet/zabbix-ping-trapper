90426b50db0624cbd1d88de11872b8f1edd5ec18
Ejayz <32452006+Ejayz@users.noreply.github.com>
2026-01-15 04:57:25 +0800

    Refactor log file handling and update config
    Simplified log file writing logic in libs/log_file.js by using a write stream and ensuring the logs directory exists. Added Config/configuration.json to version control and updated .gitignore to stop ignoring configuration.json.

--- libs/log_file.js
+++ libs/log_file.js
@@ -5,39 +5,13 @@ dotenv.config();
 const LOGFILE = process.env.LOGFILE || "./logs/logs.log";
 
 const log_file = async (data) => {
-  if (!FileSystem.existsSync(LOGFILE)) {
-    FileSystem.appendFileSync(LOGFILE, data + "\n", (err) => {
-      if (err) {
-        console.error("Error writing to log file:", err);
-      }
-    });
-  } else {
-    FileSystem.readFile(LOGFILE, "utf8", (err, fileData) => {
-      if (err) {
-        console.error("Error reading log file:", err);
-        return;
-      }
-      const content = fileData.split("\n");
-      const date = content[0].split("-")[0];
-      if (Number((new Date() - new Date(date)) / (1000 * 60 * 60 * 24)) >= 5) {
-        // Clear log file if older than 7 days
-        FileSystem.writeFile(LOGFILE, data, (err) => {
-          if (err) {
-            console.error("Error clearing log file:", err);
-          }
-        });
-      }
-      FileSystem.appendFile(
-        LOGFILE,
-        data ? "\n" + data : "\n" + getCurrentTimestamp() + " Log entry",
-        (err) => {
-          if (err) {
-            console.error("Error writing to log file:", err);
-          }
-        }
-      );
-    });
+  if (!FileSystem.existsSync("./logs")) {
+    FileSystem.mkdirSync("./logs");
   }
+
+  const logStream = FileSystem.createWriteStream(LOGFILE, { flags: "a" });
+  logStream.write(data + "\n");
+  logStream.end();
 };
 
 module.exports = { log_file };
